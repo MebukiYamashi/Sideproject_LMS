63055590b2f56da60ee5916afd862a489f9e2f78
HanHyeJi <hjhan.0621@gmail.com>
2023-06-20 14:59:48 +0900

    add exception package (ValidationException class, Message interface)

---
+++ exception/message/Message.java
@@ -0,0 +1,7 @@
+package exception.message;
+
+public interface Message {
+    final String ERR_MSG_VALIDATION_EXCEPTION = "유효한 핸드폰번호가 아닙니다." + "\n" +
+                                                "< 01011112222 >" + "\n" +
+                                                "위와 같은 형식으로 입력해야 합니다.";
+}
